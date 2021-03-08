55206791883c915b438295d0d54aa58531079520
eli dowling <eli.jambu@yahoo.com>
2021-03-08 11:28:13 +1000

    working cross compilation

--- crossShell.nix
+++ crossShell.nix
@@ -18,10 +18,10 @@ in
     # removed without issue: crPkgs.windows.mingw_w64_pthreads crPkgs.windows.mingw_w64
     #mingw_w64 must be in nativeBuildInputs
       
-      nativeBuildInputs=[   /*crPkgs.windows.mingw_w64*/ rust  ];
+      nativeBuildInputs=[ pkgs.clang  crPkgs.windows.mingw_w64 rust ];
      # i used to have windows.pthreads instea of mingw_w64_pthreads. I get errors at the beginning using pthreads then i get errors at the end if i don't
      #crPkgs.windows.pthreads must be in buildinputs
-      buildInputs=[        /* crPkgs.windows.pthreads */ crPkgs.windows.mingw_w64_pthreads      ];
+      buildInputs=[          crPkgs.windows.pthreads   crPkgs.windows.mingw_w64_pthreads      ];
       
     }
     
