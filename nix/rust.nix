4dd0c94ced3f5525dee488bc88fcb933fb5faee4
Eli Dowling <eli.jmambu@yahoo.com>
2021-10-06 19:06:00 +1000

    update nix environment

--- nix/rust.nix
+++ nix/rust.nix
@@ -5,7 +5,7 @@ let
   pkgs =
     import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
  
-  chan = pkgs.rust-bin.nightly."2021-01-01".rust.override{ 
+  chan = pkgs.rust-bin.nightly."2021-09-06".rust.override{ 
   extensions = [ "rust-src" "rust-analysis" ];
   targets = [ "x86_64-pc-windows-gnu" "x86_64-unknown-linux-gnu" ];};
 in chan
